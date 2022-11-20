6f16a9fe5b935bfad640999eafeeda9b86a1223c
Dyaika <91870391+Dyaika@users.noreply.github.com>
2022-11-20 12:44:24 +0300

    Анимированная сырна

--- src/com/company/AnimationApp.java
+++ src/com/company/AnimationApp.java
@@ -4,24 +4,57 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AnimationApp extends JFrame {
-    private Image[] images;
-    private JLabel label;
+    private ImageIcon[] images;
+    private JPanel panel;
+    private JLabel curImageLabel;
+    private static int frame = 0;
+
+    //грузим картинки в массив
     public void loadImages(){
-        images = new Image[15];
+        images = new ImageIcon[15];
+        var cwd = System.getProperty("user.dir");
         for (int i = 1; i < 16; i++){
-            images[i-1] = new ImageIcon(i + ".jpg").getImage();
+            images[i-1] = new ImageIcon(cwd + "/src/com/company/" + i + ".jpg");
         }
     }
 
     public AnimationApp() throws HeadlessException {
-        super("Animation");
-        setLayout(null);
-        setSize(700, 350);
+        super("Показываем картинку");
+
         loadImages();
-        label = new JLabel(new ImageIcon(images[0]));
-        add(label);
+        panel = new JPanel();
+        loadImages();
+        setSize(300, 300);
+        curImageLabel = new JLabel(images[frame]);
+        panel.add(curImageLabel);
+        add(panel);
+        // помещаем окно в центре экрана
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setVisible(true);
+    }
+
+    //смена 1 кадра
+    public void changeFrame(){
+        frame++;
+        if (frame>= 15){
+            frame = 0;
+        }
+        curImageLabel.setIcon(images[frame]);
+        repaint();
+    }
+
+    //анимация
+    public void kino(){
+        while (true){
+
+            try {
+                Thread.sleep(60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            changeFrame();
+        }
     }
 }
